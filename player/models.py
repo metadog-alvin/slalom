f7c1b56274733c4fbe7358ac1d9c6ece72c0f31e
Alvin <alvin@adpost.com.tw>
2022-04-21 23:20:19 +0800

    tmp

---
+++ player/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Player(models.Model):
+    identity = models.CharField(max_length=10,null=True)
+    name = models.CharField(max_length=50,null=True)
+    gender = models.CharField(max_length=1, null=True)
+    city = models.CharField(max_length=10, null=True)
+    agency = models.CharField(max_length=50, null=True)
+    created_by = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'players'
